4d534ac149ebf4c32574b71cd03a79df5a4e96b2
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-14 00:46:13 +0530

    working...note we do not use setContentView....used default slides

--- app/src/main/java/rm/iamjosephvarghese/androidwalkthrough/IntroActivity.java
+++ app/src/main/java/rm/iamjosephvarghese/androidwalkthrough/IntroActivity.java
@@ -1,22 +1,20 @@
 package rm.iamjosephvarghese.androidwalkthrough;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
-public class MainActivity extends AppIntro {
+public class IntroActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
 
-        Intent walkThrough = new Intent(MainActivity.this,IntroActivity.class);
-        startActivity(walkThrough);
+//        We are not using set content view
 
+        addSlide(AppIntroFragment.newInstance("title","description", R.drawable.ic_arrow_back_white,getResources().getColor(R.color.colorAccent)));
     }
 }
